66b81930d67973526c7585614e9c5032dfff63be
brian corker <bcorker123@gmail.com>
2022-04-26 18:05:38 -0400

    commit tree b21be45aab2a56d5ccf5b8641fc44e99a847e78e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let messagesArray = [];
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        messagesArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+    return messagesArray;
+}
+
+function countDown(int){
+    let i = 0;
+    while (int > -1) {
+        console.log(int)
+        int--
+        i++
+    }
+}
